ea73df78abc9866171b1217a9945ff6c0cfb6251
Lisa S <52213257+lisasce@users.noreply.github.com>
2020-02-26 13:09:46 +0100

    petstore

---
+++ petstore/src/app/products.ts
@@ -0,0 +1,27 @@
+export const
+  products = [{
+    img: 'catFood.JPG',
+    name: "Whiskas",
+    price: 4.40
+  },{
+    img: 'catFood1.JPG',
+    name: "Wilderness",
+    price: 12.60
+  },{
+    img: "catFood2.JPG",
+    name: "Vintage Cat Food",
+    price: 6.20
+  },{
+    img: "catFood3.JPG",
+    name: "Meow Mix",
+    price: 3.40
+  },{
+    img: "catFood4.JPG",
+    name: "Friskies",
+    price: 3.90
+  },{
+    img: "catFood5.JPG",
+    name: "Leaps&bounds",
+    price: 4.40
+  }
+  ];
